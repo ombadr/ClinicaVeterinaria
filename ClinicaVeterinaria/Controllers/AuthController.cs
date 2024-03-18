aeab1052754a7274dfc7be3a2deda91865bb75bf
Omar Badr <omarbadr938@gmail.com>
2024-03-18 12:06:48 +0100

    fix register and login

--- ClinicaVeterinaria/Controllers/AuthController.cs
+++ ClinicaVeterinaria/Controllers/AuthController.cs
@@ -1,22 +1,90 @@
-﻿using System;
+﻿using ClinicaVeterinaria.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ClinicaVeterinaria.Controllers
 {
     public class AuthController : Controller
     {
 
+        DBContext db = new DBContext();
+
         public ActionResult Login()
         {
+            if (User.Identity.IsAuthenticated) return RedirectToAction("Index", "Home");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Utenti user, bool? keepLogged)
+        {
+            bool keepUserLogged = keepLogged.HasValue && keepLogged.Value;
+            var loggedUser = db.Utenti.FirstOrDefault(u => u.Email == user.Email);
+            if (loggedUser != null)
+            {
+                bool validPassword = BCrypt.Net.BCrypt.Verify(user.PasswordHash, loggedUser.PasswordHash);
+                if (validPassword)
+                {
+                    FormsAuthentication.SetAuthCookie(loggedUser.Id.ToString(), keepUserLogged);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            TempData["ErrorLogin"] = true;
+            return RedirectToAction("Login");
         }
 
         public ActionResult Register()
         {
+
+            if (User.Identity.IsAuthenticated) return RedirectToAction("Index", "Home");
             return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingUser = db.Utenti.FirstOrDefault(u => u.Email == model.Email);
+                if (existingUser == null)
+                {
+                    var hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.PasswordHash);
+
+                    var newUser = new Utenti
+                    {
+                        Nome = model.Nome,
+                        Cognome = model.Cognome,
+                        Email = model.Email,
+                        PasswordHash = hashedPassword,
+                        Ruolo = "Farmacista"
+                    };
+                    db.Utenti.Add(newUser);
+                    db.SaveChanges();
+
+                    FormsAuthentication.SetAuthCookie(newUser.Id.ToString(), false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "An account with this email already exists.");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- ClinicaVeterinaria/Models/RegisterViewModel.cs
+++ ClinicaVeterinaria/Models/RegisterViewModel.cs
@@ -8,6 +8,7 @@ namespace ClinicaVeterinaria.Models
     public class RegisterViewModel
     {
         public string Nome { get; set; }
+        public string Cognome { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string Ruolo { get; set; }
